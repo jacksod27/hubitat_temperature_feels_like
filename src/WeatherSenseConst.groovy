289acb7d0cbee71b82e24e6c237ebf01f17fce22
jacksod27 <45473189+jacksod27@users.noreply.github.com>
2026-02-21 17:04:28 +1100

    Update WeatherSenseConst.groovy
    replaced static final with final

--- src/WeatherSenseConst.groovy
+++ src/WeatherSenseConst.groovy
@@ -8,43 +8,43 @@
 */
 
 // Domain / name
-static final String WEATHERSENSE_DOMAIN = "weathersense"
-static final String WEATHERSENSE_NAME   = "WeatherSense"
+final String WEATHERSENSE_DOMAIN = "weathersense"
+final String WEATHERSENSE_NAME   = "WeatherSense"
 
 // Configuration options
-static final String CONF_TEMPERATURE_SENSOR        = "temperature_sensor"
-static final String CONF_HUMIDITY_SENSOR           = "humidity_sensor"
-static final String CONF_WIND_SPEED_SENSOR         = "wind_speed_sensor"
-static final String CONF_PRESSURE_SENSOR           = "pressure_sensor"
-static final String CONF_IS_OUTDOOR                = "is_outdoor"
-static final String CONF_SOLAR_RADIATION_SENSOR    = "solar_radiation_sensor"
-static final String CONF_TIME_OF_DAY               = "time_of_day"
-static final String CONF_DISPLAY_UNIT              = "display_unit"
-static final String CONF_WIND_DIRECTION_SENSOR     = "wind_direction_sensor"
-static final String CONF_WIND_DIRECTION_CORRECTION = "wind_direction_correction"
-static final String CONF_SMOOTHING_ENABLED         = "smoothing_enabled"
-static final String CONF_SMOOTHING_FACTOR          = "smoothing_factor"
+final String CONF_TEMPERATURE_SENSOR        = "temperature_sensor"
+final String CONF_HUMIDITY_SENSOR           = "humidity_sensor"
+final String CONF_WIND_SPEED_SENSOR         = "wind_speed_sensor"
+final String CONF_PRESSURE_SENSOR           = "pressure_sensor"
+final String CONF_IS_OUTDOOR                = "is_outdoor"
+final String CONF_SOLAR_RADIATION_SENSOR    = "solar_radiation_sensor"
+final String CONF_TIME_OF_DAY               = "time_of_day"
+final String CONF_DISPLAY_UNIT              = "display_unit"
+final String CONF_WIND_DIRECTION_SENSOR     = "wind_direction_sensor"
+final String CONF_WIND_DIRECTION_CORRECTION = "wind_direction_correction"
+final String CONF_SMOOTHING_ENABLED         = "smoothing_enabled"
+final String CONF_SMOOTHING_FACTOR          = "smoothing_factor"
 
 // Default values
-static final String DEFAULT_NAME                    = "Feels Like Temperature"
-static final Boolean DEFAULT_IS_OUTDOOR             = true
-static final BigDecimal DEFAULT_SMOOTHING_FACTOR    = 0.3G
+final String DEFAULT_NAME                    = "Feels Like Temperature"
+final Boolean DEFAULT_IS_OUTDOOR             = true
+final BigDecimal DEFAULT_SMOOTHING_FACTOR    = 0.3G
 
 // Comfort levels (short form)
-static final String COMFORT_EXTREME_COLD   = "extreme_cold"
-static final String COMFORT_VERY_COLD      = "very_cold"
-static final String COMFORT_COLD           = "cold"
-static final String COMFORT_COOL           = "cool"
-static final String COMFORT_SLIGHTLY_COOL  = "slightly_cool"
-static final String COMFORT_COMFORTABLE    = "comfortable"
-static final String COMFORT_SLIGHTLY_WARM  = "slightly_warm"
-static final String COMFORT_WARM           = "warm"
-static final String COMFORT_HOT            = "hot"
-static final String COMFORT_VERY_HOT       = "very_hot"
-static final String COMFORT_EXTREME_HOT    = "extreme_hot"
+final String COMFORT_EXTREME_COLD   = "extreme_cold"
+final String COMFORT_VERY_COLD      = "very_cold"
+final String COMFORT_COLD           = "cold"
+final String COMFORT_COOL           = "cool"
+final String COMFORT_SLIGHTLY_COOL  = "slightly_cool"
+final String COMFORT_COMFORTABLE    = "comfortable"
+final String COMFORT_SLIGHTLY_WARM  = "slightly_warm"
+final String COMFORT_WARM           = "warm"
+final String COMFORT_HOT            = "hot"
+final String COMFORT_VERY_HOT       = "very_hot"
+final String COMFORT_EXTREME_HOT    = "extreme_hot"
 
 // Comfort level descriptions
-static final Map<String, String> COMFORT_DESCRIPTIONS = [
+final Map<String, String> COMFORT_DESCRIPTIONS = [
     extreme_cold:    "Extreme Cold Stress",
     very_cold:       "Very Strong Cold Stress",
     cold:            "Strong Cold Stress",
@@ -59,7 +59,7 @@ static final Map<String, String> COMFORT_DESCRIPTIONS = [
 ]
 
 // Comfort level detailed explanations
-static final Map<String, String> COMFORT_EXPLANATIONS = [
+final Map<String, String> COMFORT_EXPLANATIONS = [
     extreme_cold:    "Extreme risk: frostbite possible in less than 5 minutes",
     very_cold:       "High risk: frostbite possible in 5-10 minutes",
     cold:            "Warning: frostbite possible in 10-30 minutes",
@@ -74,7 +74,7 @@ static final Map<String, String> COMFORT_EXPLANATIONS = [
 ]
 
 // Comfort icons
-static final Map<String, String> COMFORT_ICONS = [
+final Map<String, String> COMFORT_ICONS = [
     extreme_cold:       "mdi:snowflake-alert",
     very_cold:          "mdi:snowflake-thermometer",
     cold:               "mdi:thermometer-low",
@@ -89,16 +89,16 @@ static final Map<String, String> COMFORT_ICONS = [
 ]
 
 // Sensor attributes (Hubitat camelCase)
-static final String ATTR_COMFORT_LEVEL             = "comfortLevel"
-static final String ATTR_COMFORT_DESCRIPTION       = "comfortDescription"
-static final String ATTR_COMFORT_EXPLANATION       = "comfortExplanation"
-static final String ATTR_CALCULATION_METHOD        = "calculationMethod"
-static final String ATTR_TEMPERATURE               = "temperature"
-static final String ATTR_HUMIDITY                  = "humidity"
-static final String ATTR_WIND_SPEED                = "windSpeed"
-static final String ATTR_PRESSURE                  = "pressure"
-static final String ATTR_IS_OUTDOOR                = "isOutdoor"
-static final String ATTR_TIME_OF_DAY               = "timeOfDay"
-static final String ATTR_IS_COMFORTABLE            = "isComfortable"
-static final String ATTR_WIND_DIRECTION            = "windDirection"
-static final String ATTR_WIND_DIRECTION_CORRECTION = "windDirectionCorrection"
+final String ATTR_COMFORT_LEVEL             = "comfortLevel"
+final String ATTR_COMFORT_DESCRIPTION       = "comfortDescription"
+final String ATTR_COMFORT_EXPLANATION       = "comfortExplanation"
+final String ATTR_CALCULATION_METHOD        = "calculationMethod"
+final String ATTR_TEMPERATURE               = "temperature"
+final String ATTR_HUMIDITY                  = "humidity"
+final String ATTR_WIND_SPEED                = "windSpeed"
+final String ATTR_PRESSURE                  = "pressure"
+final String ATTR_IS_OUTDOOR                = "isOutdoor"
+final String ATTR_TIME_OF_DAY               = "timeOfDay"
+final String ATTR_IS_COMFORTABLE            = "isComfortable"
+final String ATTR_WIND_DIRECTION            = "windDirection"
+final String ATTR_WIND_DIRECTION_CORRECTION = "windDirectionCorrection"
