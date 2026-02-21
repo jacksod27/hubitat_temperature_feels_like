267610ef665728d6773eecd4790e1ef010bb717d
jacksod27 <45473189+jacksod27@users.noreply.github.com>
2026-02-21 17:05:05 +1100

    Update WeatherSenseCalculator.groovy
    Replaced static final with final

--- src/WeatherSenseCalculator.groovy
+++ src/WeatherSenseCalculator.groovy
@@ -7,9 +7,9 @@
  Source:   [https://github.com/smkrv/ha-weathersense](https://github.com/smkrv/ha-weathersense)
 */
 
-static final BigDecimal STANDARD_PRESSURE = 101.3G  // kPa
+final BigDecimal STANDARD_PRESSURE = 101.3G  // kPa
 
-static final Map COMFORT_LEVELS = [
+final Map COMFORT_LEVELS = [
     "extreme_cold": "extreme_cold",
     "very_cold": "very_cold", 
     "cold": "cold",
